d43f7ac42b1150e6bf06c487a85cec75a233f56a
sarthakpant772 <sarthak.pant31@gmail.com>
2023-03-16 09:37:31 +0530

    check

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import SpeechToText from './components/SpeechToText'
 import TextToSpeech from './components/TextToSpeech'
-
+import { store } from './app/store'
+import { Provider } from 'react-redux'
 const App = () => {
   return (
     <div>
-      <SpeechToText />
+      <Provider store={store}>
+        <SpeechToText />
+      </Provider>
     </div>
   )
 }
---
+++ client/src/features/book/bookSlice.js
@@ -0,0 +1,32 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  value: 0,
+  author: '',
+  name: '',
+}
+
+export const bookSlice = createSlice({
+  name: 'book',
+  initialState,
+  reducers: {
+    increment: (state) => {
+      // Redux Toolkit allows us to write "mutating" logic in reducers. It
+      // doesn't actually mutate the state because it uses the Immer library,
+      // which detects changes to a "draft state" and produces a brand new
+      // immutable state based off those changes
+      state.value += 1
+    },
+    decrement: (state) => {
+      state.value -= 1
+    },
+    incrementByAmount: (state, action) => {
+      state.value += action.payload
+    },
+  },
+})
+
+// Action creators are generated for each case reducer function
+export const { increment, decrement, incrementByAmount } = bookSlice.actions
+
+export default bookSlice.reducer
